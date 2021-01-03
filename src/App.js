218ece58398a00ff63a8e53cae7ae6a07e494437
ZoeHYH <zoehyhtw@gmail.com>
2021-01-04 00:13:09 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./components/Header";
 import LoginPage from "./page/LoginPage";
 
 function App() {
-  let isLogin = true;
+  let isLogin = false;
   return (
     <Router>
       {isLogin ? (
