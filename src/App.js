c61ded2a23d9cc124e17bd8e60261c28ca2021a9
ZoeHYH <zoehyhtw@gmail.com>
2021-01-04 00:16:03 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./components/Header";
 import LoginPage from "./page/LoginPage";
 
 function App() {
