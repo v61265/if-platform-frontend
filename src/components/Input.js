e98f28196bab8328c552c9e63525b707d75f7dfc
ZoeHYH <zoehyhtw@gmail.com>
2021-01-31 14:03:03 +0800

    refine component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { FrontIcon } from "./Image";
 import { StyledPfc } from "./Text";
 import Down from "../svg/down.svg";
@@ -8,19 +8,123 @@ const InputWrapper = styled.div`
   position: relative;
 `;
 
-const StyledInput = styled.input`
+const StyledShadow = css`
+  box-shadow: ${({ theme }) => theme.shadow};
+`;
+
+const StyledBorder = css`
+  border: 1px solid ${({ theme }) => theme.color.black};
+`;
+
+const StyledCustomInput = css`
   ${StyledPfc}
   background: ${({ theme }) => theme.color.white};
   color: ${({ theme }) => theme.color.black};
   width: 100%;
   border-radius: 10px;
   padding: ${({ theme }) => theme.space.sm}px;
-  box-shadow: ${({ theme }) => theme.shadow};
 `;
 
-const StyledIconInput = styled(StyledInput)`
+const StyledCustomSelect = css`
+  appearance: none;
+  background: ${({ theme }) => theme.color.white} url(${Down}) right center
+    no-repeat;
+  background-size: ${({ theme }) => theme.icon.md}px;
+  &:required:invalid {
+    color: gray;
+  }
+`;
+
+const IconPadding = css`
   padding-left: ${({ theme }) =>
     theme.icon.md + theme.space.sm + theme.font.xs}px;
+`;
+
+const StyledInput = styled.input`
+  ${StyledCustomInput}
+  ${StyledBorder}
+`;
+
+export const Input = ({
+  type,
+  name,
+  placeholder,
+  value,
+  handleValue,
+  required,
+}) => {
+  const handleOnChange = ({ target }) => {
+    handleValue(target.name, target.value);
+  };
+  return (
+    <StyledInput
+      as="input"
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleOnChange}
+      required={required}
+    />
+  );
+};
+Input.propTypes = {
+  type: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  handleValue: PropTypes.func,
+  required: PropTypes.bool,
+};
+
+const StyledSelect = styled.select`
+  ${StyledCustomInput}
+  ${StyledBorder}
+  ${StyledCustomSelect}
+`;
+export const Select = ({
+  name,
+  placeholder,
+  options,
+  value,
+  handleValue,
+  required,
+}) => {
+  const handleOnChange = ({ target }) => {
+    handleValue(target.name, target.value);
+  };
+  return (
+    <StyledSelect
+      as="select"
+      name={name}
+      value={value}
+      onChange={handleOnChange}
+      required={required}
+    >
+      <option value="" disabled hidden>
+        {placeholder}
+      </option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.name}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+};
+Select.propTypes = {
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+  value: PropTypes.string,
+  handleValue: PropTypes.func,
+  required: PropTypes.bool,
+};
+
+const StyledIconInput = styled.input`
+  ${StyledCustomInput}
+  ${StyledShadow}
+  ${IconPadding}
 `;
 export const IconInput = ({
   type,
@@ -57,15 +161,12 @@ IconInput.propTypes = {
   handleValue: PropTypes.func,
 };
 
-const StyledIconSelect = styled(StyledIconInput)`
-  appearance: none;
-  background: ${({ theme }) => theme.color.white} url(${Down}) right center
-    no-repeat;
-  background-size: ${({ theme }) => theme.icon.md}px;
-  &:required:invalid {
-    color: gray;
-  }
+const StyledIconSelect = styled.select`
+  ${StyledCustomSelect}
+  ${StyledShadow}
+  ${IconPadding}
 `;
+
 export const IconSelect = ({
   name,
   placeholder,
